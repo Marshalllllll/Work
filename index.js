98fc24e45c3d9beab7997ed6e10fd4907c66dd78
Marshalllllll <karuma0606@live.jp>
2019-12-01 14:54:26 +0900

    test commit

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("index.js: loaded");
-console.log("git test");
+const userId = "Marshalllllll";
+function fetchUserInfo(userId){
+fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+        console.log(response.status); // => 200
+        //エラーレスポンスが返されたことを検知する。
+        if (!response.ok){
+            console.error("エラーレスポンス",response);
+        }else{
+        return response.json().then(userInfo => {
+            // JSONパースされたオブジェクトが渡される
+            console.log(userInfo); // =>{...}
+        });
+    }
+    }).catch(error => {
+        console.error(error);
+    });
+}
